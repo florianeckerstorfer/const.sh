905db1fe7f9b0f489e0f418787ea808db6ec4df5
Florian Eckerstorfer <florian@eckerstorfer.net>
2020-10-24 17:27:48 +0200

    Implement Emojicon tool

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layout-base.njk');
   eleventyConfig.addLayoutAlias('generic', 'layout-generic.njk');
 
+  eleventyConfig.addPassthroughCopy('site/**/*.css');
+  eleventyConfig.addPassthroughCopy('site/**/*.js');
+  eleventyConfig.addPassthroughCopy({ 'src/styles/**/*.css': 'styles' });
+  eleventyConfig.addPassthroughCopy({
+    'src/fonts/**/*.otf': 'fonts',
+    'src/fonts/**/*.ttf': 'fonts',
+    'src/fonts/**/*.woff': 'fonts',
+    'src/fonts/**/*.woff2': 'fonts',
+  });
+
   return {
     dir: {
       input: 'site',
---
+++ site/emojicon/main.js
@@ -0,0 +1,47 @@
+function main() {
+  const emojicon = document.querySelector('#emojicon');
+  const input = emojicon.querySelector('#emoji');
+  const canvas = emojicon.querySelector('#canvas');
+  const dataUrl = emojicon.querySelector('#data-url');
+  const context = canvas.getContext('2d');
+  context.font = '96px sans-serif';
+
+  function handleValue(value) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (!value || value.length === 0) {
+      dataUrl.value = '';
+      return;
+    }
+
+    const measure = context.measureText(value);
+    const positionX = (canvas.width - measure.width) / 2;
+    const positionY = measure.actualBoundingBoxAscent;
+    context.fillText(value, positionX, positionY);
+
+    dataUrl.value = canvas.toDataURL('image/png');
+  }
+
+  emojicon.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleValue(input.value);
+  });
+
+  input.addEventListener('input', (event) => {
+    handleValue(event.target.value);
+  });
+
+  emojicon.querySelectorAll('.examples a').forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      input.value = event.target.innerHTML;
+      handleValue(input.value);
+    });
+  });
+
+  dataUrl.addEventListener('click', (event) => {
+    event.preventDefault();
+    dataUrl.select();
+  });
+}
+
+main();
