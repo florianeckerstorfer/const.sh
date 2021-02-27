283b9a0b3d159ea7efd2309f6ef5d3456fada82d
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-02-27 15:14:58 +0100

    covid19-at: Show number of tests

--- site/covid19-at/index.11tydata.js
+++ site/covid19-at/index.11tydata.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
 const neatCsv = require('neat-csv');
 const dayjs = require('dayjs');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+
+dayjs.extend(customParseFormat);
 
 function uniqueProvince(value, index, self) {
   return (
@@ -13,11 +16,78 @@ async function getTimeline() {
     'https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv'
   );
   const text = await response.text();
-  return neatCsv(text, { separator: ';' });
+  return (await neatCsv(text, { separator: ';' })).map((row) => ({
+    ...row,
+    BundeslandID: parseInt(row.BundeslandID, 10),
+    AnzEinwohner: parseInt(row.AnzEinwohner, 10),
+    AnzahlFaelle: parseInt(row.AnzahlFaelle, 10),
+    AnzahlFaelleSum: parseInt(row.AnzahlFaelleSum, 10),
+    AnzahlFaelle7Tage: parseInt(row.AnzahlFaelle7Tage, 10),
+    SiebenTageInzidenzFaelle: parseInt(row.SiebenTageInzidenzFaelle, 10),
+    AnzahlTotTaeglich: parseInt(row.AnzahlTotTaeglich, 10),
+    AnzahlTotSum: parseInt(row.AnzahlTotSum, 10),
+    AnzahlGeheiltTaeglich: parseInt(row.AnzahlGeheiltTaeglich, 10),
+    AnzahlGeheiltSum: parseInt(row.AnzahlGeheiltSum, 10),
+  }));
+}
+
+async function getTestsAndHospitals() {
+  const response = await fetch(
+    'https://covid19-dashboard.ages.at/data/CovidFallzahlen.csv'
+  );
+  const text = await response.text();
+  const data = await neatCsv(text, { separator: ';' });
+  const newData = {};
+  for (let i = 0; i < data.length; i += 1) {
+    const date = dayjs(data[i].Meldedat, 'DD.MM.YYYY').format('YYYY-MM-DD');
+    if (!newData[date]) {
+      newData[date] = {};
+    }
+    const BundeslandID = parseInt(data[i].BundeslandID, 10);
+    const dayBefore = dayjs(data[i].Meldedat, 'DD.MM.YYYY')
+      .subtract(1, 'day')
+      .format('DD.MM.YYYY');
+    const dayBeforeRow = data.find((item) => {
+      return (
+        item.Meldedat === dayBefore &&
+        item.BundeslandID === data[i].BundeslandID
+      );
+    });
+    const testsYesterdayGesamt = dayBeforeRow ? dayBeforeRow.TestGesamt : 0;
+    const TestGesamt = parseInt(data[i].TestGesamt, 10);
+    const Test = TestGesamt - testsYesterdayGesamt;
+    newData[date][BundeslandID] = {
+      ...data[i],
+      BundeslandID,
+      TestGesamt,
+      Test,
+      FZHosp: parseInt(data[i].FZHosp, 10),
+      FZICU: parseInt(data[i].FZICU, 10),
+      FZHospFree: parseInt(data[i].FZHospFree, 10),
+      FZICUFree: parseInt(data[i].FZICUFree, 10),
+      BundeslandID: parseInt(data[i].BundeslandID, 10),
+    };
+  }
+  return newData;
 }
 
 module.exports = async function () {
-  const timeline = await getTimeline();
+  const testsAndHospitals = await getTestsAndHospitals();
+  const timeline = (await getTimeline()).map((row) => {
+    const rowDate = dayjs(row.Time, 'DD.MM.YYYY HH:mm:ss').format('YYYY-MM-DD');
+    const found = testsAndHospitals[rowDate]
+      ? testsAndHospitals[rowDate][row.BundeslandID]
+      : undefined;
+    if (found) {
+      row.Test = found.Test;
+      row.TestGesamt = found.TestGesamt;
+      row.FZHosp = found.FZHosp;
+      row.FZICU = found.FZICU;
+      row.FZHospFree = found.FZHospFree;
+      row.FZICUFree = found.FZICUFree;
+    }
+    return row;
+  });
   const provinces = timeline
     .map(({ Bundesland, BundeslandID }) => ({ Bundesland, BundeslandID }))
     .filter(uniqueProvince);
@@ -27,5 +97,5 @@ module.exports = async function () {
     lastWeek: dayjs().subtract(8, 'day').format('DD.MM.YYYY'),
     lastMonth: dayjs().subtract(31, 'day').format('DD.MM.YYYY'),
   };
-  return { timeline, provinces, dates };
+  return { timeline, testsAndHospitals, provinces, dates };
 };
