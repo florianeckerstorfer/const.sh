0449f5557dfb72ea67fb3bc5a852893000f41a8f
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-02-27 22:59:05 +0100

    Auto-deploy on 14:30 every day

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync dist/ s3://const.sh --profile const.sh
-aws cloudfront create-invalidation --distribution-id E2GYU54JQ5UGHQ --paths '/*' --profile const.sh
+aws s3 sync dist/ s3://const.sh --profile const.sh --region "eu-central-1"
+aws cloudfront create-invalidation --distribution-id E2GYU54JQ5UGHQ --paths '/*' --profile const.sh --region "eu-central-1"
